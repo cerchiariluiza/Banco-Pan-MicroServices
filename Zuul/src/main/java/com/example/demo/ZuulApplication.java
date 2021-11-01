2598e7393f7d67a8f01bdd206eda396ee2ef98de
cerchiariluiza <docente.luiza@gmail.com>
2021-10-31 22:09:10 -0300

    5 config zuul e postman

--- Zuul/src/main/java/com/example/demo/ZuulApplication.java
+++ Zuul/src/main/java/com/example/demo/ZuulApplication.java
@@ -2,7 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+
+
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
 
